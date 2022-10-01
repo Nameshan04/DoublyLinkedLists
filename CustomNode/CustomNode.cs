33bd57f5c9fc755a26999d8332d9f2f43858380a
Nameshan04 <nameshanmoonsamy@gmail.com>
2022-10-01 16:15:40 +0200

    initial commit

---
+++ CustomNode/CustomNode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedLists
+{
+    public class CustomNode
+    {
+        //double forwards and backs
+        public CustomNode Next;
+        public CustomNode Prev;
+        public string data;
+
+        public CustomNode()
+        {
+            //good practice
+        }
+
+        public CustomNode(string data)
+        {
+            this.data = data;
+        }
+
+
+    }
+}
